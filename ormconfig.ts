d7f82f4c753f9eeb17274a09d52bc66eb9310820
Deepak Terse <iamdeepakterse@gmail.com>
2021-02-27 18:30:23 +0530

    Log correction

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log(process.env.DB_PORT);
 
 module.exports = {
     "type": "mysql",
--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,6 @@ createConnection().then(async connection => {
     // run app
     app.listen(process.env.SERVER_PORT);
 
-    console.log("Express application is up and running on port 3000");
+    console.log("Express application is up and running on port ", process.env.SERVER_PORT);
 
 }).catch(error => console.log("TypeORM connection error: ", error));
